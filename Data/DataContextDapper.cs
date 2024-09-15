7452f18cab4e57148b6a34786d899a8ed9134164
Derrick Strong <derrickmstrong@gmail.com>
2024-09-15 16:25:38 -0500

    Initial Commit

---
+++ Data/DataContextDapper.cs
@@ -0,0 +1,60 @@
+namespace HelloWorld.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using Dapper;
+    using Microsoft.Data.SqlClient;
+
+    public class DataContextDapper {
+        // create connection to the database
+        private string _connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=false;Uid=sa;Pwd=SQLConnect1;";
+
+        // query method - returns IEnumerable of T type of data
+        public IEnumerable<T> LoadData<T>(string sql) {
+            // create a connection to the database
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+
+            // query the database
+            var query =  dbConnection.Query<T>(sql);
+
+            // return the results
+            return query;
+        }
+        // query single method - returns single object of T type of data
+        public T LoadDataSingle<T>(string sql) {
+            // create a connection to the database
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+
+            // query the database
+            var query =  dbConnection.QuerySingle<T>(sql);
+
+            // return the results
+            return query;
+        }
+        // execute method - executes sql
+        public bool ExecuteSql(string sql)
+        {
+            // create a connection to the database
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+
+            // Execute the sql
+            var execute = dbConnection.Execute(sql);
+
+            return execute > 0;
+        }
+        // execute method - executes sql
+        public int ExecuteSqlWithRowCount(string sql)
+        {
+            // create a connection to the database
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+
+            // Execute the sql
+            var execute = dbConnection.Execute(sql);
+
+            return execute;
+        }
+
+    }
+}
---
+++ Models/Computer.cs
@@ -0,0 +1,14 @@
+// Models - This is a class that defines the structure of an object
+namespace HelloWorld.Models
+{
+    public class Computer
+    {
+        public string Motherboard { get; set; } = "";
+        public int CPUCores { get; set; }
+        public bool HasWifi { get; set; }
+        public bool HasLTE { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; } = "";
+    }
+}
